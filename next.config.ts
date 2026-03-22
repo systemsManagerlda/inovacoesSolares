8aa74b53ac396421022b49e162f9594a260b313f
systemsManagerlda <systemsmanagerlda@gmail.com>
2026-03-22 15:23:03 +0200

    commit 2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["inovacoessolares.co.mz"], // <--- adicione seu domínio aqui
+    // Substitua `domains` por `remotePatterns` (recomendado)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'woo.inovacoessolares.co.mz',
+        port: '',
+        pathname: '/wp-content/uploads/**',  // Especifica o caminho das imagens do Woo
+      },
+    ],
+    
+    // Adicione as qualidades que você está usando
+    qualities: [75, 85, 90],
   },
 };
 
